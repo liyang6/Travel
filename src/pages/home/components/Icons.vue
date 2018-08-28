4855265057bee1cecddae85194e66ab17e2a532e
liyang <liyang368@126.com>
2018-08-28 17:54:45 +0800

    add iocns

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="pageCont of pages">
+        <div class="icon" v-for="item in pageCont" :key="item.id">
+          <div class="icon_img">
+            <img class="icon_img_cont" :src="item.imgUrl">
+          </div>
+          <p class="icon_desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: "icons",
+  data() {
+    return {
+      iconList: [{
+          id: "0001",
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票"
+        },
+        {
+          id: "0002",
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png",
+          desc: "北京必游"
+        },
+        {
+          id: "0003",
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/6a/45f595250c73d102.png",
+          desc: "夏日玩水"
+        },
+        {
+          id: "0004",
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png",
+          desc: "动植物园"
+        },
+        {
+          id: "0005",
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/6a/45f595250c73d102.png",
+          desc: "夏日玩水"
+        },
+        {
+          id: "0006",
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png",
+          desc: "故宫"
+        },
+        {
+          id: "0007",
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/6a/45f595250c73d102.png",
+          desc: "夏日玩水"
+        },
+        {
+          id: "0008",
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png",
+          desc: "一日游"
+        },
+        {
+          id: "0009",
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1804/95/8d02011d149bdb02.png",
+          desc: "汽车票"
+        }
+      ]
+    }
+  },
+  computed: {
+    pages() {
+      const pages = [];
+      this.iconList.forEach( (item, index) => {
+        const page = Math.floor(index / 8);
+        if ( !pages[page] ) {
+          pages[page] = [];
+        }
+        pages[page].push(item);
+
+      })
+      return pages
+    }
+  }
+}
+
+</script>
+
+<style scoped lang="stylus">
+@import '~styles/varibles.styl'
+@import '~styles/mixins.styl'
+
+.icons>>>.swiper-container {
+  height: 0;
+  padding-bottom: 50%;
+}
+
+.icons {
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 50%;
+}
+
+.icon {
+  position: relative;
+  float: left;
+  width: 25%;
+  height: 0;
+  padding-bottom: 25%;
+  overflow: hidden;
+}
+
+.icon_img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: .44rem;
+  padding: .1rem;
+  box-sizing: border-box;
+}
+
+.icon_img_cont {
+  display: block;
+  margin: 0 auto;
+  height: 100%;
+}
+
+.icon_desc {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: .44rem;
+  line-height: .44rem;
+  text-align: center;
+  color: $dark;
+  ellipsis()
+}
+
+</style>
