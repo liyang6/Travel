4dae9313d41934530c3625c97686978af4319075
liyang <liyang368@126.com>
2018-08-29 12:43:45 +0800

    add search to city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,14 +1,17 @@
 <template>
 	<div>
 		<city-header></city-header>
+		<city-search></city-search>
 	</div>
 </template>
 <script type="text/javascript">
 	import CityHeader from './components/CityHeader'
+	import CitySearch from './components/CitySearch'
 	export default {
 		name:"city",
 		components:{
-			CityHeader
+			CityHeader,
+			CitySearch
 		}
 	}
 </script>
---
+++ src/pages/city/components/CitySearch.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="search">
+		<input class="search-input" type="text" placeholder="输入汉字或拼音" name="">
+	</div>
+</template>
+<script type="text/javascript">
+	export default {
+		name:"citySearch"
+	}
+</script>
+
+<style type="text/css" scoped lang="stylus">
+@import '~styles/varibles.styl'
+.search{
+	height: .72rem;
+	padding:0 .1rem;
+	background:$bgColor;
+}
+.search-input{
+	box-sizing: border-box;
+	padding:0 .1rem;
+	height: .62rem;
+	line-height: .62rem;
+	width: 100%;
+	text-align: center;
+	border-radius: .06rem;	
+	color: #666;
+}
+</style>
