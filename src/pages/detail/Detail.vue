1ba99a8b5ebefe5029fc89b5526ce294507ca4bc
liyang <liyang368@126.com>
2018-09-05 20:02:49 +0800

    add detail-header

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 <script type="text/javascript">
 import DetailBanner from './components/Banner'	
+import DetailHeader from './components/Header'	
 export default {
   name: "detail",
   components:{
-  	DetailBanner
+  	DetailBanner,
+  	DetailHeader
   }
 }
 
 </script>
-<style type="text/css"></style>
+<style type="text/css">
+	.content{
+		height: 50rem;
+	}
+</style>
